a8c4a865e47d6ac4a73931fc2fe4cbd2432acf0a
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2018-10-06 01:08:24 +0800

    01aggregate

--- LinqInner/LinqInner/Program.cs
+++ LinqInner/LinqInner/Program.cs
@@ -13,5 +13,23 @@ namespace LinqInner
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
+        static IEnumerable<string> GetSequenceFromConsole()
+        {
+            var input = string.Empty;
+            while (input != "end!")
+            {
+                input = Console.ReadLine();
+                yield return input;
+            }
+        }
+
+    }
+
+    static class LinqExtension
+    {
+        public static TAggregate Aggregate<TAggregate, TSource>(this IEnumerable<TSource> source, Func<TSource, TAggregate, TAggregate> func)
+        {
+            return default(TAggregate);
+        }
     }
 }
