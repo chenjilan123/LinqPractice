74350db80b2707a416e01866c6e393a1e7f55f7d
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2018-10-06 15:50:14 +0800

    1

---
+++ LinqInner/LinqInner/LinqExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqInner
+{
+    public static class LinqExtension
+    {
+        public static IEnumerable<T> OrderBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> comparer)
+            where TKey : IComparable<TKey>
+        {
+            //return new ICustomOrderedEnumerable<T, TKey>(source, comparer);
+            return new CustomOrderedEnumerable<T, TKey>(source, comparer);
+        }
+        public static IEnumerable<T> ThenBy<T, TKey>(this IOrderingImpl<T> source, Func<T, TKey> comparer)
+            where TKey : IComparable<TKey>
+        {
+            return new CustomOrderedEnumerable<T, TKey>(source, comparer);
+        }
+    }
+}
--- LinqInner/LinqInner/Program.cs
+++ LinqInner/LinqInner/Program.cs
@@ -20,21 +20,17 @@ namespace LinqInner
             }
             Console.ReadKey();
         }
+        static IEnumerable<string> GetSequenceFromConsole()
+        {
+            var input = string.Empty;
+            input = Console.ReadLine();
+            while (input != "end!")
+            {
+                yield return input;
+                input = Console.ReadLine();
+            }
+        }
     }
 
-    static class LinqExtension
-    {
-        public static IEnumerable<T> OrderBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> comparer)
-            where TKey : IComparable<TKey>
-        {
-            //return new ICustomOrderedEnumerable<T, TKey>(source, comparer);
-            return new CustomOrderedEnumerable<T, TKey>(source, comparer);
-        }
-        public static IEnumerable<T> ThenBy<T, TKey>(this IOrderingImpl<T> source, Func<T, TKey> comparer)
-            where TKey : IComparable<TKey>
-        {
-            return new CustomOrderedEnumerable<T, TKey>(source, comparer);
-        }
-    }
 
 }
