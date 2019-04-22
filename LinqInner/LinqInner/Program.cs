26518d37705ff1ab387493b053c632a80eb641f1
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2019-04-23 01:14:11 +0800

    1

--- LinqInner/LinqInner/Program.cs
+++ LinqInner/LinqInner/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,12 @@ namespace LinqInner
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
+
+        private static void SimpleExpressionTree()
+        {
+            Expression<Func<int, bool>> lambda = num => num < 5;
+        }
+
         static IEnumerable<string> GetSequenceFromConsole()
         {
             var input = string.Empty;
