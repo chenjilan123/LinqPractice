4daef8f9d5756821239a7aeca8e34bf105e9c8b7
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2019-05-01 18:47:56 +0800

    1

--- LinqInner/LinqInner/Program.cs
+++ LinqInner/LinqInner/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,9 @@ namespace LinqInner
     {
         static void Main(string[] args)
         {
+            DynamicExpressionTree();
+            return;
+
             //Console.WriteLine(System.Type.FilterAttribute.ToString());
             //Console.WriteLine(System.Type.FilterName);
             //Console.WriteLine(System.Type.FilterNameIgnoreCase);
@@ -30,9 +34,175 @@ namespace LinqInner
 
             //AndAlsoModifier.RunDemo();
 
-            BuildDynamicQuery.RunDemo();
-            Console.ReadKey();
+            //BuildDynamicQuery.RunDemo();
+            //Console.ReadKey();
         }
+
+        #region ExpressionTree
+        private static void ExpressionTree()
+        {
+            // 1
+            Expression<Action<int>> expr = i => Console.WriteLine(i);
+            //Wrong
+            //Expression<Action<int>> expr = i => { Console.WriteLine(i); };
+            Action<int> action1 = i => { Console.WriteLine(i); };
+            Action<int> action2 = i => Console.WriteLine(i);
+            Console.WriteLine(expr);
+            expr.Compile()(5);
+
+            // 2
+            ParameterExpression param1 = Expression.Parameter(typeof(int), "i");
+            ParameterExpression param2 = Expression.Parameter(typeof(int), "i");
+            MethodCallExpression methodCall1 = Expression.Call(
+                typeof(Console).GetMethod("WriteLine", new Type[] { typeof(int) }),
+                param1);
+            var expr2 = Expression.Lambda<Action<int>>(methodCall1, param1);
+            //Wrong
+            //var expr2 = Expression.Lambda<Action<int>>(methodCall, param2);
+            expr2.Compile()(2);
+
+            // 3.Block
+            MethodCallExpression methodCall2 = Expression.Call(
+                typeof(Console).GetMethod("WriteLine", new Type[] { typeof(int) }),
+                Expression.Constant(500));
+            var block = Expression.Block(methodCall1, methodCall2, methodCall2);
+            var expr3 = Expression.Lambda<Action<int>>(
+                block,
+                new ParameterExpression[] { param1 });
+            expr3.Compile()(50);
+
+        }
+        #endregion
+
+        #region DynamicExpressionTree
+        private static void DynamicExpressionTree()
+        {
+            var func1 = ETFact();
+            var i = func1(5);
+            Console.WriteLine(i);
+
+            var func2 = ILFact();
+            var j = func2(6);
+            Console.WriteLine(j);
+        }
+
+        //ExpressionTree°æ±¾
+        private static Func<int, int> ETFact()
+        {
+            var value = Expression.Parameter(typeof(int), "value");
+            var result = Expression.Variable(typeof(int), "result");
+            var label = Expression.Label(typeof(int));
+            var block = Expression.Block(
+                new[] { result }
+                , Expression.Assign(result, Expression.Constant(1))
+                , Expression.Loop(Expression.IfThenElse(
+                    Expression.GreaterThan(value, Expression.Constant(1))
+                    , Expression.MultiplyAssign(result, Expression.PostDecrementAssign(value))
+                    , Expression.Break(label, result))
+                    , label)
+                );
+            return Expression.Lambda<Func<int, int>>(block, value).Compile();
+        }
+
+        //MSIL°æ±¾
+        static Func<int, int> ILFact()
+
+        {
+
+            var method = new DynamicMethod(
+
+      "factorial", typeof(int),
+
+            new[] { typeof(int) }
+
+            );
+
+            var il = method.GetILGenerator();
+
+            var result = il.DeclareLocal(typeof(int));
+
+            var startWhile = il.DefineLabel();
+
+            var returnResult = il.DefineLabel();
+
+
+
+
+            // result = 1
+
+            il.Emit(OpCodes.Ldc_I4_1);
+
+            il.Emit(OpCodes.Stloc, result);
+
+
+
+
+            // if (value <= 1) branch end
+
+            il.MarkLabel(startWhile);
+
+            il.Emit(OpCodes.Ldarg_0);
+
+            il.Emit(OpCodes.Ldc_I4_1);
+
+            il.Emit(OpCodes.Ble_S, returnResult);
+
+
+
+
+            // result *= (value¨C)
+
+            il.Emit(OpCodes.Ldloc, result);
+
+            il.Emit(OpCodes.Ldarg_0);
+
+            il.Emit(OpCodes.Dup);
+
+            il.Emit(OpCodes.Ldc_I4_1);
+
+            il.Emit(OpCodes.Sub);
+
+            il.Emit(OpCodes.Starg_S, 0);
+
+            il.Emit(OpCodes.Mul);
+
+            il.Emit(OpCodes.Stloc, result);
+
+
+
+
+            // end while
+
+            il.Emit(OpCodes.Br_S, startWhile);
+
+
+
+
+            // return result
+
+            il.MarkLabel(returnResult);
+
+            il.Emit(OpCodes.Ldloc, result);
+
+            il.Emit(OpCodes.Ret);
+
+
+
+
+            return (Func<int, int>)method.CreateDelegate(typeof(Func<int, int>));
+
+        }
+
+        private static int CSharpFact(int value)
+        {
+            int result = 1;
+            while (value > 1)
+            {
+                result *= value--;
+            }
+            return result;
+        }
+        #endregion
 
         #region Intial
         private static void SimpleExpressionTree()
